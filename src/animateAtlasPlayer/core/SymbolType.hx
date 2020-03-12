9944bd575819b1b7215a3222ff12993d53d66b68
ISART\m.anthoine <mathieu.anthoine@lilo.org>
2020-03-12 19:07:12 +0100

    Fix bug SymbolType Graphic

--- src/animateAtlasPlayer/core/SymbolType.hx
+++ src/animateAtlasPlayer/core/SymbolType.hx
@@ -3,7 +3,7 @@ package animateAtlasPlayer.core;
 class SymbolType
 {
     
-    public static inline var GRAPHIC : String = "graphic";
+    public static inline var GRAPHIC : String = "G";
     public static inline var MOVIE_CLIP : String = "MC";
     public static inline var BUTTON : String = "button";
     
